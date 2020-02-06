040e431844465643424df4e9fef23e8350013a86
anedepa <nazotora@outlook.jp>
2020-02-07 00:19:54 +0900

    version up

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -3,6 +3,7 @@ class TopPageController < ApplicationController
   def index
     @per = session[:per] || 5
     @articles = Article.paginate(page: params[:page],per_page: @per).order(created_at:'desc')
+    
   end
 
   def new  
